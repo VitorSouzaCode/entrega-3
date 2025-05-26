9f6a8c2815f723862eb0990ba33454d62e943b14
Vitor de Souza da Conceição <mysticrocha67@gmail.com>
2025-05-26 16:55:00 -0400

    Delete back-end directory

--- back-end/src/middlewares/verificar-perfil-organizador.ts
+++
@@ -1,6 +0,0 @@
-import { Perfil } from "../entidades/usuario"; 
-export default function verificarPerfilOrganizador(request, response, next) { 
-
-  if (request.perfil === Perfil.ORGANIZADOR) return next(); 
-  else return response.status(401).json({ erro: "Acesso não autorizado." });
-}
